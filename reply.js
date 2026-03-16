9601b5d3c6db41fec7f59dc1f110c07a42c2586d
da <ddaasshh1234@gmail.com>
2026-03-16 23:47:24 +0900

    add

---
+++ reply.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: "messageCreate",
+  async execute(message) {
+
+    if (message.author.bot) return;
+
+    if (message.content === "あ") {
+
+      const replies = [
+        "こんにちは",
+        "やあ",
+        "元気？",
+        "どうした？",
+        "呼んだ？",
+        "はい？"
+      ];
+
+      const randomReply = replies[Math.floor(Math.random() * replies.length)];
+
+      message.reply(randomReply);
+    }
+
+  }
+};
