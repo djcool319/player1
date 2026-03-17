230be8e728fcbe4a7c0b8a3d7a6fabf1b7a78755
da <ddaasshh1234@gmail.com>
2026-03-17 21:06:04 +0900

    add

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,27 +1,29 @@
-require('dotenv').config();
+// deploy-commands.js
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId } = require('./config.json');
-const fs = require('node:fs');
+require('dotenv').config();
+const fs = require('fs');
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter(f => f.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
-	try {
-		//console.log(`Started refreshing ${commands.length} application (/) commands.`);
-		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    console.log('Refreshing commands...');
+
+    const data = await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands }
+    );
+
+    console.log(`Successfully reloaded ${data.length} commands.`);
+  } catch (err) {
+    console.error(err);
+  }
 })();
